1175a5599a58705e0ae25d088aeb544894052415
Carlos Feria <carlosthe19916@gmail.com>
2018-02-12 15:35:46 -0500

    add messages to wrap xml files

--- src/main/java/org/clarksnut/batchs/commons/EndBatchlet.java
+++ src/main/java/org/clarksnut/batchs/commons/EndBatchlet.java
@@ -4,7 +4,9 @@ import org.jboss.logging.Logger;
 
 import javax.batch.api.AbstractBatchlet;
 import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.context.JobContext;
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
@@ -13,9 +15,12 @@ public class EndBatchlet extends AbstractBatchlet {
 
     private static final Logger logger = Logger.getLogger(EndBatchlet.class);
 
+    @Inject
+    private JobContext jobCtx;
+
     @Override
     public String process() {
-        logger.info("Ending batchlet...");
+        logger.info("Ending batchlet..." + jobCtx.getJobName());
         return BatchStatus.COMPLETED.toString();
     }
 
