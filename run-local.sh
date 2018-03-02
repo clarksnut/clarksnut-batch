dd772b7f5be25ea0219e72fae478dfaf6bc269bb
Carlos Feria <carlosthe19916@gmail.com>
2018-03-02 08:34:16 -0500

    change roles

--- run-local.sh
+++ run-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mvn clean package -DskipTests
-java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006 target/clarksnut-*-swarm.jar -Dswarm.port.offset=100 -Dsso.auth.server.url="$CLARKSNUT_SSO_SERVER_URL" -Dsso.clientSecret="$CLARKSNUT_CLIENT_MAIL_COLLECTOR_CLIENT_SECRET" -Dswarm.project.stage=peru
+java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006 target/clarksnut-*-swarm.jar -Dswarm.port.offset=100 -Dsso.auth.server.url="http://localhost:8081/auth" -Dsso.clientSecret="$1" -Dswarm.project.stage=peru
 
